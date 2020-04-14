d2b6da943e79c015eaf0e3ca0d0cc85fcd5d59c1
panupongjkn <panupong.jkn@gmail.com>
2020-04-14 16:26:00 +0700

    [UPDATE] change database

--- database/database.go
+++ database/database.go
@@ -1,19 +1,13 @@
 package database
 
 import (
-	"be_nms/models"
-
 	"github.com/jinzhu/gorm"
 )
 
 func Open() *gorm.DB {
-	db, err := gorm.Open("mysql", "sql12331330:mynBjQxz6q@tcp(sql12.freemysqlhosting.net:3306)/sql12331330?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root@tcp(localhost:3306)/test?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	}
-	// Migrate the schema
-	db.AutoMigrate(&models.User{})
-	user := models.User{FName: "Panupong", LName: "Joknoi", Email: "panupong.jkn@gmail.com", LineID: "Ufc12c85816992da6381aa3405b9e8083"}
-	db.Create(&user)
 	return db
 }
