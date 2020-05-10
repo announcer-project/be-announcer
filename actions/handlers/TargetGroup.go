2ae651b8bfc9aa17c262023edcc21378af6d2056
panupongjkn <panupong.jkn@gmail.com>
2020-05-11 00:23:30 +0700

    [CREATE] func Create TargetGroup and Get AllTargetGroup

--- actions/handlers/TargetGroup.go
+++ actions/handlers/TargetGroup.go
@@ -1,11 +1,23 @@
 package handlers
 
 import (
+	"be_nms/actions/repositories"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func CreateTargetGroup(c echo.Context) error {
-	return c.JSON(http.StatusOK, "test")
+	_, err := repositories.CreateTargetGroup(c)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, "Create success.")
+}
+func GetAllTargetGroup(c echo.Context) error {
+	targetGroups, err := repositories.GetAllTargetGroup(c)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, targetGroups)
 }
---
+++ actions/repositories/TargetGroup.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"be_nms/database"
+	"be_nms/models"
+	"be_nms/models/modelsMember"
+	"errors"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CreateTargetGroup(c echo.Context) (interface{}, error) {
+	authorization := c.Request().Header.Get("Authorization")
+	jwt := string([]rune(authorization)[7:])
+	tokens, _ := DecodeJWT(jwt)
+	db := database.Open()
+	defer db.Close()
+	admin := models.Admin{}
+	db.Where("user_id = ? AND system_id = ?", tokens["user_id"], c.FormValue("systemid")).Find(&admin)
+	if admin.ID == 0 {
+		return nil, errors.New("You not admin in this system.")
+	}
+	system := models.System{}
+	db.Where("id = ?", c.FormValue("systemid")).Find(&system)
+	if system.ID == 0 {
+		return nil, errors.New("Not have system.")
+	}
+	targetGroup := modelsMember.TargetGroup{TargetGroupName: c.FormValue("targetgroupname"), NumberOfMembers: 0, SystemID: system.ID}
+	db.Create(&targetGroup)
+	if targetGroup.ID == 0 {
+		return nil, errors.New("Create fail.")
+	}
+	return targetGroup, nil
+}
+func GetAllTargetGroup(c echo.Context) (interface{}, error) {
+	authorization := c.Request().Header.Get("Authorization")
+	jwt := string([]rune(authorization)[7:])
+	tokens, _ := DecodeJWT(jwt)
+	db := database.Open()
+	defer db.Close()
+	admin := models.Admin{}
+	db.Where("user_id = ? AND system_id = ?", tokens["user_id"], c.FormValue("systemid")).Find(&admin)
+	if admin.ID == 0 {
+		return nil, errors.New("You not admin in this system.")
+	}
+	targetGroups := []modelsMember.TargetGroup{}
+	db.Where("system_id = ?", c.FormValue("systemid")).Find(&targetGroups)
+	return targetGroups, nil
+}
