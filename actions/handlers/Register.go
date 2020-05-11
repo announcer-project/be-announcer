1c2fe6b789442811d5f8471584b09a4031d4e712
panupongjkn <panupong.jkn@gmail.com>
2020-05-11 07:08:58 +0700

    [UPDATE] func Register

--- actions/handlers/Register.go
+++ actions/handlers/Register.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"be_nms/database"
+	"be_nms/actions/repositories"
 	"be_nms/models"
 	"net/http"
 
@@ -9,16 +9,10 @@ import (
 )
 
 func Register(c echo.Context) error {
-	user := models.User{}
-	db := database.Open()
-	db.Where("email", c.FormValue("email")).First(&user)
-	if user.ID == "" {
-		user.CreateUser(c.FormValue("fname"), c.FormValue("lname"), c.FormValue("email"), c.FormValue("line"), c.FormValue("facebook"), c.FormValue("google"))
-		db.Create(&user)
-		if user.ID != "" {
-			return c.JSON(http.StatusBadRequest, "Create Fail")
-		}
-		return c.JSON(http.StatusOK, "Create Success")
+	user, err := repositories.Register(c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
-	return c.JSON(http.StatusBadRequest, "You have account")
+	jwt := repositories.EncodeJWT(user.(models.User))
+	return c.JSON(http.StatusOK, jwt)
 }
--- actions/repositories/Register.go
+++ actions/repositories/Register.go
@@ -1,9 +1,24 @@
 package repositories
 
 import (
+	"be_nms/database"
+	"be_nms/models"
+	"errors"
+
 	"github.com/labstack/echo/v4"
 )
 
-func Register(c echo.Context) (bool, error) {
-	return true, nil
+func Register(c echo.Context) (interface{}, error) {
+	user := models.User{}
+	db := database.Open()
+	db.Where("email = ? OR line_id = ?", c.FormValue("email"), c.FormValue("line")).First(&user)
+	if user.ID != "" {
+		return user, errors.New("You have account.")
+	}
+	user.CreateUser(c.FormValue("fname"), c.FormValue("lname"), c.FormValue("email"), c.FormValue("line"), c.FormValue("facebook"), c.FormValue("google"))
+	db.Create(&user)
+	if user.ID == "" {
+		return nil, errors.New("Register fail.")
+	}
+	return user, nil
 }
