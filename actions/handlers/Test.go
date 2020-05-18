74932ccbbc542594ba1e444af1a5da2b4fd0e38a
panupongjkn <panupong.jkn@gmail.com>
2020-05-19 00:50:39 +0700

    [UPDATE] add image

---
+++ actions/handlers/Test.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func CheckUser(c echo.Context) error {
+	// authorization := c.Request().Header.Get("Authorization")
+	// jwt := string([]rune(authorization)[7:])
+	// tokens, _ := DecodeJWT(jwt)
+	// db := database.Open()
+	// defer db.Close()
+	// user := models.
+	// db. Where("id = ?", tpkens["user_id"])
+	return nil
+}
