105d3a8eaaafe42561b1a41369e7638812ea241b
panupongjkn <panupong.jkn@gmail.com>
2020-07-09 23:52:22 +0700

    [UPDATE] Mailer

--- actions/repositories/Mailer.go
+++ actions/repositories/Mailer.go
@@ -1,7 +1,6 @@
 package repositories
 
 import (
-	"crypto/tls"
 	"log"
 
 	"gopkg.in/gomail.v2"
@@ -26,7 +25,6 @@ func SendEmail(Subject string, email string, otp string) error {
 	m.SetBody("text/html", html)
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, getEnv("MAILER_EMAIL", ""), getEnv("MAILER_PASSWORD", ""))
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	log.Print(getEnv("MAILER_EMAIL", ""))
 	log.Print(getEnv("MAILER_PASSWORD", ""))
 	if err := d.DialAndSend(m); err != nil {
