74942673001a1addb98c20041fb88b8888b4e496
panupongjkn <panupong.jkn@gmail.com>
2020-09-16 01:19:22 +0700

    [UPDATE] api target group

--- actions/handlers/TargetGroup.go
+++ actions/handlers/TargetGroup.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"be_nms/actions/repositories"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -15,9 +16,18 @@ func CreateTargetGroup(c echo.Context) error {
 	return c.JSON(http.StatusOK, "Create success.")
 }
 func GetAllTargetGroup(c echo.Context) error {
+	var message struct {
+		Message string `json:"message"`
+	}
+	log.Print(c.QueryParam("systemid"))
+	if c.QueryParam("systemid") == "" {
+		message.Message = "not have query param."
+		return c.JSON(400, message)
+	}
 	targetGroups, err := repositories.GetAllTargetGroup(c.QueryParam("systemid"))
 	if err != nil {
-		return err
+		message.Message = err.Error()
+		return c.JSON(500, message)
 	}
 	return c.JSON(http.StatusOK, targetGroups)
 }
